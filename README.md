64488a567da9df0fd38258de49baca1b33f44d67
Femi Ajayi <fajayiacademic.rrc.ca>
2026-03-12 12:19:52 -0500

    Clean version without large files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Real-Time-Football-Player-Tactics-Tracking-Using-Deep-Learning
+This is a deep‑learning project that tracks football players in real time and highlights team movements and tactics. It detects players, follows their positions, and gives a clear view of how teams shape up and move on the pitch.
---
+++ yolo_inference.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+model = YOLO('yolo26x')
+
+results = model.predict('input_video/E3c993bd2_0 (74).mp4', save=True)
+print(results[0])
+print('-----------------------------')
+for box in results[0].boxes:
+    print(box)  # Bounding box coordinates
+
+x = 5 
+y = 10
+z = x + y
+print(z)
