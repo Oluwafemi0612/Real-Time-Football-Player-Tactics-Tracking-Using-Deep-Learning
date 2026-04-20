cdac1895f3beef8505341d2de39276f2bcdfe01a
Femi Ajayi <fajayiacademic.rrc.ca>
2026-04-20 12:51:19 -0500

    Add solution folder only

--- utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from.video_util import read_video, save_video
--- yolo_inference.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-model = YOLO('models/best.pt')  # Load a pretrained YOLOv8m model
-
-results = model.predict('input_videos/E3c993bd2_0 (74).mp4', save=True)
-print(results[0])
-print('-----------------------------')
-for box in results[0].boxes:
-    print(box)  # Bounding box coordinates
